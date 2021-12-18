2f5edd74d6438e84e35620caaf139b833e16ca70
msekioka <m.sekioka.xx@gmail.com>
2021-12-18 12:22:49 +0900

    スムーススクロール・ローディングアニメーションの記述追加

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-$(function(){
-
-  $('.burger-btn').on('click',function(){
-    if($(this).attr('aria-expanded') == 'false'){
-      $(this).attr('aria-expanded',true);
-      $('#js-global-menu').attr('aria-hidden',false)
-      $('.nav-menu-list').css('display','flex');
-    } else {
-      $(this).attr('aria-expanded',false);
-      $('#js-global-menu').attr('aria-hidden',true)
-      $('.nav-menu-list').css('display','none');
-    }
+// バーガーボタン・スライドメニュー
+$('.burger-btn').on('click',function(){
+  $('body').toggleClass('is-drawerActive');
+  if($(this).attr('aria-expanded') == 'false'){
+    $(this).attr('aria-expanded',true);
+    $('.nav-menu-bg').attr('aria-hidden',false)
+    $('.nav-menu-list').css('display','flex');
+  } else {
+    $(this).attr('aria-expanded',false);
+    $('.nav-menu-bg').attr('aria-hidden',true)
+    $('.nav-menu-list').css('display','none');
+  }
   });
 
-});
-
 // ヘッダー追従
-
 function fixedHeader(){
   let height = $('#top').outerHeight();
   let scroll = $(this).scrollTop();
@@ -26,10 +23,42 @@ function fixedHeader(){
   };
 };
 
+// ローディングアニメーション
+function loadingAnimation(){
+  $('.loading-area').delay(1500).fadeOut('slow');
+};
+
+//スムーススクロール
+$('a[href^="#"]').click(function(e){
+  let speed = 500;
+  let href = $(this).attr('href');
+  let target = $(href == '#' || href == '' ? 'html' : href);
+  let position = target.offset().top - 30;
+  $('html').animate({
+    scrollTop: position
+  },speed,'swing');
+
+  if($('body').hasClass('is-drawerActive')){
+    $('body').removeClass('is-drawerActive');
+    $('.burger-btn').attr('aria-expanded',false);
+    $('.nav-menu-list').css('display','none');
+    $('.nav-menu-bg').attr('aria-hidden',true);
+  }
+  return false;
+});
+
+
+//---------------------------//
+// 各種　関数の発火タイミング定義 //
+//---------------------------//
+
+// スクロール時のイベントハンドラ
 $(window).on('scroll', function(){
   fixedHeader();
 });
 
+// 読み込み完了時のイベントハンドラ
 $(window).on('load', function(){
+  loadingAnimation();
   fixedHeader();
 });
